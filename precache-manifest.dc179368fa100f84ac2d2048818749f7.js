e78fd4b8f9729470d8fa69448eb53c09b7a024e7
Victor Anogwih <anogwihvictor@gmail.com>
2024-03-11 00:34:21 -0400

    UI Upload

---
+++ precache-manifest.dc179368fa100f84ac2d2048818749f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ab0c61d669c4870170fb767204782a9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cad8124b30466a92a45a",
+    "url": "/static/css/2.7ae7b8b0.chunk.css"
+  },
+  {
+    "revision": "5b1b34e71810b112a3fe",
+    "url": "/static/css/main.59e98df8.chunk.css"
+  },
+  {
+    "revision": "cad8124b30466a92a45a",
+    "url": "/static/js/2.1fe66383.chunk.js"
+  },
+  {
+    "revision": "5b1b34e71810b112a3fe",
+    "url": "/static/js/main.e1d96c01.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
